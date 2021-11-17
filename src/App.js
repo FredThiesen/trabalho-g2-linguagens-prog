d4ee61b76d62836aab9f4211a5919487b9b4584a
Fred <ricardofredt@hotmail.com>
2021-11-16 23:04:14 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import musk from "./assets/elon-musk-bw.png"
+import "./App.css"
+import React, { useState } from "react"
+import {
+	base64Computer,
+	base64Television,
+	base64Oven,
+	base64Headphone,
+	base64Fridge,
+} from "./mock"
+
+const mockImages = [
+	base64Computer,
+	base64Television,
+	base64Oven,
+	base64Headphone,
+	base64Fridge,
+]
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [product, setProduct] = useState(null)
+	const handleFetchProduct = () => {
+		// fetch("https://api.github.com/users/elonmusk")
+		// 	.then((response) => response.json())
+		// 	.then((data) => setProduct(data))
+		setProduct({
+			image: base64Computer,
+		})
+	}
+	const handleNextProduct = () => {
+		// rotate through mockImages
+		const index = mockImages.indexOf(product.image)
+		let nextIndex = index + 1
+
+		if (nextIndex > mockImages.length - 1) {
+			nextIndex = 0
+		}
+		const nextImage = mockImages[nextIndex]
+		setProduct({
+			image: nextImage,
+		})
+
+		// const nextImage = mockImages.filter(
+		// 	(image) => image !== product.image[0]
+		// )
+		// setProduct({ image: nextImage })
+	}
+	return (
+		<div className="App">
+			<header className="App-header">
+				<div className="App-top-container">
+					<img src={musk} className="App-logo" alt="logo" />
+					<button onClick={handleFetchProduct}>Buscar Produto</button>
+				</div>
+				<div className="App-product-container">
+					{product && (
+						<div className="Product-container">
+							<img src={product.image} />
+							<button onClick={handleNextProduct}>
+								Próximo produto
+							</button>
+						</div>
+					)}
+				</div>
+			</header>
+		</div>
+	)
 }
 
-export default App;
+export default App
